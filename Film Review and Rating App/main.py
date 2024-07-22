9383daa3960557a50517d82047951e6f027eb3a6
İrem Bağçıvan <230601013@bakircay.edu.tr>
2024-07-22 20:16:41 +0300

    list films by year

--- Film Review and Rating App/main.py
+++ Film Review and Rating App/main.py
@@ -7,7 +7,7 @@ class Film():
         self.rating = rating
 
     def __str__(self):
-            return f"Title: {self.title}, Director: {self.director}, Year: {self.release_year}, Rating: {self.rating}\nReview: {self.review}"
+            return f"Title: {self.title}, Director: {self.director}, Year: {self.release_year}, Rating: {self.rating}, Review: {self.review}"
 
 class Film_Review():
     def __init__(self):
@@ -20,19 +20,21 @@ class Film_Review():
         review = input("Review: ")
         rating = float(input("Rating: "))
         film = Film(title,director,release_year,review,rating)
+        print("")
         self.films.append(film)
 
     def list_films(self):
         if not self.films:
             print("No films added yet!")
+            print("")
             return
         else:
             for film in self.films:
                 print(film)
+                print("")
 
     def search_films_by_titles(self):
         title = input("Enter the title to search for: ")
-
         found_films =[]
 
         for film in self.films:
@@ -42,9 +44,26 @@ class Film_Review():
         if found_films:
             for film in found_films:
                 print(film)
+                print("")
         else:
             print("No films found!")
-                  
+            print("")
+
+    def list_films_by_year(self):
+        year = int(input("Enter the release year to list films: "))
+        found_films = []
+
+        for film in self.films:
+            if film.release_year == year:
+                found_films.append(film)
+        
+        if found_films:
+            for film in found_films:
+                print(film)
+                print("")
+        else:
+            print("No films found for that year!")
+            print("")
              
          
     def menu(self):
@@ -52,7 +71,8 @@ class Film_Review():
                 print("1. Add a film")
                 print("2. List all films")
                 print("3. Search films by title")
-                print("6. Exit")
+                print("4. List films by release year")
+                print("5. Exit")
                 choice = input("Choose an option: ")
 
                 if choice == '1':
@@ -60,8 +80,10 @@ class Film_Review():
                 elif choice == '2':
                     self.list_films()
                 elif choice == '3':
-                    self.search_films_by_title()
-                elif choice == '6':
+                    self.search_films_by_titles()
+                elif choice == '4':
+                    self.list_films_by_year()
+                elif choice == '5':
                     print("Exiting the application.")
                     break
                 else:
